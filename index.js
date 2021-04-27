e405a1d21826a3cd35217c6612d3a4817aac070d
Kateřina Levová <katerina.levova@gmail.com>
2021-04-27 12:46:08 +0200

    Uprava symbolu, kdo je na tahu

--- index.js
+++ index.js
@@ -4,32 +4,30 @@ console.log('funguju!');
 
 let naTahu = 'circle';
 
-
-
 const kliknuti = (event) => {
-    if (naTahu === 'circle') {
-        event.target.classList.add('board__field--circle');
-        document.querySelector('.hraje').innerHTML = '<h2>HRAJE: </h2> <img class="kolecko" src="images/circle.svg" alt="znak kolecko">';
-        event.target.disabled = true;
-        naTahu = 'cross';
-    } else {
-        event.target.classList.add('board__field--cross');
-        document.querySelector('.hraje').innerHTML = '<h2>HRAJE: </h2> <img class="krizek" src="images/cross.svg" alt="znak krizek">';
-        event.target.disabled = true;
-        naTahu = 'circle';
-    }
-
-}
+  if (naTahu === 'circle') {
+    event.target.classList.add('board__field--circle');
+    document.querySelector('.hraje').innerHTML =
+      '<h2>HRAJE: </h2> <img class="kolecko" src="images/circle.svg" alt="znak kolecko">';
+    event.target.disabled = true;
+    naTahu = 'cross';
+    document.querySelector('.hraje').innerHTML =
+      '<h2>HRAJE: </h2> <img class="krizek" src="images/cross.svg" alt="znak krizek">';
+  } else {
+    event.target.classList.add('board__field--cross');
+    document.querySelector('.hraje').innerHTML =
+      '<h2>HRAJE: </h2> <img class="krizek" src="images/cross.svg" alt="znak krizek">';
+    event.target.disabled = true;
+    naTahu = 'circle';
+    document.querySelector('.hraje').innerHTML =
+      '<h2>HRAJE: </h2> <img class="kolecko" src="images/circle.svg" alt="znak kolecko">';
+  }
+};
 
 const items = document.querySelectorAll('.policko');
 
 for (let i = 0; i < items.length; i += 1)
-    items[i].addEventListener('click', kliknuti);
-
-
-
-
-
+  items[i].addEventListener('click', kliknuti);
 
 //------------------------------------------------------------
 
